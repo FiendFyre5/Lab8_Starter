37ef762ae4bd4f63385515e5861786676f499b1e
FiendFyre5 <laverma@ucsd.edu>
2021-11-14 21:02:10 -0800

    readme and unit

--- README.md
+++ README.md
@@ -5,5 +5,6 @@ Within a Github action that runs whenever code is pushed : so that there's const
 testing standards so that we don't miss any cases.
 
 2) Would you use an end to end test to check if a function is returning the correct output? (yes/no) No
-3) 
+3) Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user. NO As this case could have many moving parts and there's a possibility that other app features would affect the non-related unit tests.
+4) YES In this case, we can create a simple unit test which can check that the message length is never greater than 80. This test doesn't have many moving parts, executes easily and doesn't affect other unit tests
 
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,77 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+// valid phone number
+//2 true
+test('verify numbers match', () => {
+  expect(functions.isPhoneNumber('123-456-7990')).toBe(true);
+});
+test('verify numbers match', () => {
+    expect(functions.isPhoneNumber('123-486-7990')).toBe(true);
+  });
+  test('verify numbers match', () => {
+    expect(functions.isPhoneNumber('123456khbk')).toBe(false);
+  });
+
+test('verify numbers match', () => {
+        expect(functions.isPhoneNumber('1234867990')).toBe(false);
+      });
+
+// valid email
+//2 true
+test('verify emails', () => {
+    expect(functions.isEmail('alok@yahoo.com')).toBe(true);
+});
+test('verify emails', () => {
+    expect(functions.isEmail('alok@gmail.com')).toBe(true);
+});
+test('verify emails', () => {
+    expect(functions.isEmail('9754w86438')).toBe(false);
+});
+
+test('verify emails', () => {
+expect(functions.isEmail('hkvkvkv!vkhvkhvk')).toBe(false);
+});
+
+// valid email
+//2 true
+test('verify password', () => {
+    expect(functions.isStrongPassword('letter_12')).toBe(true);
+});
+test('verify password', () => {
+    expect(functions.isStrongPassword('Password_1234')).toBe(true);
+});
+test('verify passwor', () => {
+    expect(functions.isStrongPassword('abc')).toBe(false);
+});
+test('verify passwor', () => {
+    expect(functions.isStrongPassword('12')).toBe(false);
+});
+
+//valid date
+test('verify date', () => {
+    expect(functions.isDate('12/12/2012')).toBe(true);
+});
+test('verify date', () => {
+    expect(functions.isDate('13/13/2012')).toBe(true);
+});
+test('verify date', () => {
+    expect(functions.isDate('12/122012')).toBe(false);
+});
+test('verify date', () => {
+    expect(functions.isDate('abkjbk')).toBe(false);
+});
+
+//valid hexcode
+test('verify hexcode', () => {
+    expect(functions.isHexColor('#334')).toBe(true);
+});
+test('verify hexcode', () => {
+    expect(functions.isHexColor('#334567')).toBe(true);
+});
+test('verify hexcode', () => {
+    expect(functions.isHexColor('#34')).toBe(false);
+});
+test('verify hexcode', () => {
+    expect(functions.isHexColor('#aaaaa')).toBe(false);
+});
